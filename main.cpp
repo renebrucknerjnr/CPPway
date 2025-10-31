aed59d29cc44dcf168f97d1e01b7b0d538564723
Controller714 <renebrucknerjnr@gmail.com>
2025-10-31 11:40:50 +0200

    fixed and added more keys

--- main.cpp
+++ main.cpp
@@ -40,9 +40,15 @@ int main() {
 		return 3;
 	}
 
-	std::cout << "Starting game loop..." << std::endl;
+	std::cout << "Use spacebar to pause" << std::endl;
+	std::cout << "Use C to clear" << std::endl;
+	std::cout << "Use F to fill" << std::endl;
+	std::cout << "Use T to change algorithm type" << std::endl;
+	std::cout << "Use left mouse button to place" << std::endl;
+	std::cout << "Use right mouse button to remove" << std::endl;
 	bool running = true;
 	bool paused = false;
+	bool brainType = true;
 	SDL_Event event;
 	unsigned int frameStart;
 	unsigned int frameTime;
@@ -55,39 +61,39 @@ int main() {
 	bool buffer2[80*60];
 
 	for (int i = 0; i < 4800; i++) {
-		// buffer1[i] = rand()%100 < 50 ? 1 : 0;
-		buffer1[i] = 0;
+		buffer1[i] = rand()%100 < 45 ? 1 : 0; // use with terrain
+		// buffer1[i] = 0;
 		buffer2[i] = 0;
 	}
-	buffer1[2440] = 1;
-	buffer1[2520] = 1;
-	buffer1[2600] = 1;
-	buffer1[2680] = 1;
-	buffer1[2760] = 1;
-	buffer1[2840] = 1;
-	buffer1[2920] = 1;
-	buffer1[3000] = 1;
-	buffer1[2681] = 1;
-	buffer1[2682] = 1;
-	buffer1[2683] = 1;
-	buffer1[2684] = 1;
-	buffer1[2685] = 1;
+	// buffer1[2440] = 1;
+	// buffer1[2520] = 1;
+	// buffer1[2600] = 1;
+	// buffer1[2680] = 1;
+	// buffer1[2760] = 1;
+	// buffer1[2840] = 1;
+	// buffer1[2920] = 1;
+	// buffer1[3000] = 1;
+	// buffer1[2681] = 1;
+	// buffer1[2682] = 1;
+	// buffer1[2683] = 1;
+	// buffer1[2684] = 1;
+	// buffer1[2685] = 1;
 
-	buffer1[2282] = 1;
-	buffer1[2283] = 1;
-	buffer1[2284] = 1;
-	buffer1[2285] = 1;
-	buffer1[2286] = 1;
-	buffer1[2287] = 1;
+	// buffer1[2282] = 1;
+	// buffer1[2283] = 1;
+	// buffer1[2284] = 1;
+	// buffer1[2285] = 1;
+	// buffer1[2286] = 1;
+	// buffer1[2287] = 1;
 	
-	buffer1[2207] = 1;
-	buffer1[2127] = 1;
-	buffer1[2047] = 1;
-	buffer1[1967] = 1;
+	// buffer1[2207] = 1;
+	// buffer1[2127] = 1;
+	// buffer1[2047] = 1;
+	// buffer1[1967] = 1;
 
-	buffer1[2367] = 1;
-	buffer1[2447] = 1;
-	buffer1[2527] = 1;
+	// buffer1[2367] = 1;
+	// buffer1[2447] = 1;
+	// buffer1[2527] = 1;
 
 	while (running) {
 		frameStart = SDL_GetTicks();
@@ -104,18 +110,22 @@ int main() {
 				if (i - 80 >= 0 && (i - 80)/80 > 0 && buffer2[i - 80] == true) kern++;
 				if (i + 80 < 80*60 && (i + 80)/80 < 60 && buffer2[i + 80] == true) kern++;
 
-				if ((i - 1 >= 0 && (i - 1)%80 != 79 && buffer2[i - 1] == true) && (i - 80 >= 0 && (i - 80)/80 > 0 && buffer2[i - 80] == true)) kern++;
-				if ((i - 1 >= 0 && (i - 1)%80 != 79 && buffer2[i - 1] == true) && (i + 80 < 80*60 && (i + 80)/80 < 60 && buffer2[i + 80] == true)) kern++;
-				if ((i + 1 < 80*60 && (i + 1)%80 != 0 && buffer2[i + 1] == true) && (i - 80 >= 0 && (i - 80)/80 > 0 && buffer2[i - 80] == true)) kern++;
-				if ((i + 1 < 80*60 && (i + 1)%80 != 0 && buffer2[i + 1] == true) && (i + 80 < 80*60 && (i + 80)/80 < 60 && buffer2[i + 80] == true)) kern++;
+				// cool, but false
+				// if ((i - 1 >= 0 && (i - 1)%80 != 79 && buffer2[i - 1] == true) && (i - 80 >= 0 && (i - 80)/80 > 0 && buffer2[i - 80] == true)) kern++;
+				// if ((i - 1 >= 0 && (i - 1)%80 != 79 && buffer2[i - 1] == true) && (i + 80 < 80*60 && (i + 80)/80 < 60 && buffer2[i + 80] == true)) kern++;
+				// if ((i + 1 < 80*60 && (i + 1)%80 != 0 && buffer2[i + 1] == true) && (i - 80 >= 0 && (i - 80)/80 > 0 && buffer2[i - 80] == true)) kern++;
+				// if ((i + 1 < 80*60 && (i + 1)%80 != 0 && buffer2[i + 1] == true) && (i + 80 < 80*60 && (i + 80)/80 < 60 && buffer2[i + 80] == true)) kern++;
 
-				// { // terrain
-				// 	if (kern > 4) buffer1[i] = 1;
-				// 	if (kern < 3) buffer1[i] = 0;
-				// 	// if (kern == 0) buffer1[i] = rand()%100 < 10 ? 1 : 0;
-				// }
+				if (i - 1 >= 0 && (i - 1)%80 != 79 && i - 80 >= 0 && (i - 80)/80 > 0 && buffer2[i - 1 - 80] == true) kern++;
+				if (i - 1 >= 0 && (i - 1)%80 != 79 && (i + 80 < 80*60 && (i + 80)/80 < 60 && buffer2[i - 1 + 80] == true)) kern++;
+				if (i + 1 < 80*60 && (i + 1)%80 != 0 && (i - 80 >= 0 && (i - 80)/80 > 0 && buffer2[i + 1 - 80] == true)) kern++;
+				if (i + 1 < 80*60 && (i + 1)%80 != 0 && (i + 80 < 80*60 && (i + 80)/80 < 60 && buffer2[i + 1 + 80] == true)) kern++;
 
-				{ // conway's
+				if (brainType) { // terrain
+					if (kern > 4) buffer1[i] = 1;
+					if (kern < 3) buffer1[i] = 0;
+					// if (kern == 0) buffer1[i] = rand()%100 < 10 ? 1 : 0;
+				} else { // conway's
 					if (buffer2[i] && (kern < 2 || kern > 3)) buffer1[i] = 0;
 					if (!buffer2[i] && kern == 3) buffer1[i] = 1;
 				}
@@ -143,6 +153,18 @@ int main() {
 			case (SDL_KEYUP):
 				if (event.key.keysym.sym == SDLK_SPACE) {
 					paused = !paused;
+				} else if (event.key.keysym.sym == SDLK_t) {
+					brainType = !brainType;
+				} else if (event.key.keysym.sym == SDLK_c) {
+					for (int i = 0; i < 4800; i++) {
+						buffer1[i] = 0;
+						buffer2[i] = 0;
+					}
+				} else if (event.key.keysym.sym == SDLK_f) {
+					for (int i = 0; i < 4800; i++) {
+						buffer1[i] = rand()%100 < 45 ? 1 : 0;
+						buffer2[i] = 0;
+					}
 				}
 				break;
 
@@ -157,9 +179,6 @@ int main() {
 		SDL_RenderClear(renderer);
 
 		// draw
-		// SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
-		// SDL_RenderDrawPoint(renderer, 20, 20);
-
 		for (int i = 0; i < 4800; i++) {
 			if (!buffer1[i]) continue;
 
